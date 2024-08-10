141ab1c1ed7d0ad764c2996432947d246f3a774f
Carlos Eduardo - Jacaré Dev <jacaredev38@gmail.com>
2024-08-10 09:04:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//let idade = 38
+//let marca = {
+  //  toyota : "Corolla",
+    //chevrolet : "Corsa"
+//}
+//console.log(marca.chevrolet , marca.toyota , idade)
+// input entrada de dados 
+// output saída de dados ou entrega os dados
+
+function avisar() {
+    alert("Esse é um modelo de formulário web.")
+    alert("Esses botões não tem nenhuma ação!")
+  
+}
+let botao = document.querySelector()
+
+botao.addEventListener("click", () => {
+    avisar()
+})
